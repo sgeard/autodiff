01e785eb3fe9aa160d5dad8dcaf0402ac3394ef8
sgeard <simon@whiteowl.co.uk>
2026-03-25 20:43:13 +0000

    Initial fpm package for AVD autodiff library

---
+++ src/avd_functions.f90
@@ -0,0 +1,17 @@
+! Created by  on 21/03/2020.
+submodule (AVD) AVD_functions
+contains
+!    module pure function sin_d0(obj) result(r)
+!        type(avd_b), intent(in) :: obj
+!        type(avd_b)             ::r
+!        r%v = sin(obj%v)
+!    end function sin_d0
+!
+!    module pure function sin_d1(obj) result(r)
+!        type(avd_d1), intent(in) :: obj
+!        type(avd_d1) :: r
+!        r = Q1_f(sin_f, obj)
+!    end function sin_d1
+
+
+end submodule AVD_functions
